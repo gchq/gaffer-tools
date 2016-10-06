9ae0a575fbe932e6d3b24c180cc660e49332320a
p013570 <p013570@users.noreply.github.com>
2016-10-06 12:11:37 +0100

    Initial commit

---
+++ mini-accumulo-cluster/README.md
@@ -0,0 +1,78 @@
+Copyright 2016 Crown Copyright
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Gaffer Mini Accumulo Cluster
+============================
+
+This project allows you to easily spin up a mini accumulo cluster. There are two options
+1. Run a Cluster with a Shell as one program (cluster-with-shell)
+2. Run the cluster separately and open shells as required. (cluster and shell working together)
+
+cluster-with-shell
+------------------
+
+To use run MiniAccumuloClusterWithShellController.main() It doesn't require any arguments, however the following can be used: [directory_name] [is_temp_directory] [root_password] [instance_name]
+
+Can be run directly in your IDE
+
+or
+
+Can be built using 'mvn package' and run from the command line using:
+```bash
+java -cp target/mini-accumulo-cluster-jar-with-dependencies.jar gaffer.miniaccumulocluster.MiniAccumuloClusterWithShellController
+```
+
+When starting up it will print out something similar to:
+
+```
+Cluster started:
+    Location - <directory-started-in>miniAccumuloCLuster
+    Zookeepers - localhost:<random port number>
+    Instance name - instance
+```
+
+It will also create a store.properties file for you in the cluster directory. This can be used for connecting to the cluster via a Gaffer Accumulo store.
+Once running it will open an Accumulo shell. You can users from the shell if required.
+To stop, enter the 'quit' command in the accumulo shell.
+
+cluster
+-------
+
+This requires the same arguments as the cluster-with-shell, but it will not open a shell. It will monitor a file called shutdown within its directory. Once that file is created, it shuts down the cluster. This allows external scripts to close the cluster, they can simply 'touch $MINIACCUMULODIR/shutdown'
+To run from command line, for example
+
+```bash
+java -cp target/mini-accumulo-cluster-jar-with-dependencies.jar gaffer.miniaccumulocluster.MiniAccumuloClusterController ~/miniAccumuloCluster false password instance
+```
+
+It may be necessary to add other libraries to the accumulo classpath, to allow things like Gaffer to work. In this case you can use the following command to add libraries to the classpath.
+```bash
+java -cp target/mini-accumulo-cluster-jar-jar-with-dependencies.jar:otherFile1.jar:otherFile2.jar gaffer.miniaccumulocluster.MiniAccumuloClusterController ~/miniAccumuloCluster false password instance
+```
+
+shell
+-----
+
+This can be run in one of two ways
+
+1. Open an interactive shell
+2. Open a shell, run commands from a file, quit the shell
+
+In either case, the first argument must be the store.properties filename created by the cluster. In the second case, add another argument that points to the file containing the commands to run.
+
+```bash
+java -cp target/mini-accumulo-cluster-jar-with-dependencies.jar gaffer.miniaccumulocluster.MiniAccumuloShellController ~/miniAccumuloCluster/store.properties
+```
+
+
---
+++ mini-accumulo-cluster/src/main/java/gaffer/miniaccumulocluster/MiniAccumuloClusterWithShellController.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2016 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gaffer.miniaccumulocluster;
+
+import org.apache.accumulo.core.util.shell.Shell;
+import org.apache.accumulo.minicluster.MiniAccumuloCluster;
+import org.apache.accumulo.minicluster.MiniAccumuloConfig;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Starts a mini accumulo cluster. Can be run directly from an IDE or compiled and
+ * the main method invoked from the command line using:
+ * java -jar target/mini-accumulo-cluster-jar-with-dependencies.jar
+ *
+ * @see MiniAccumuloClusterWithShellController.Builder
+ */
+public final class MiniAccumuloClusterWithShellController {
+    public static final String DEFAULT_DIR_NAME = "miniAccumuloCluster";
+    public static final String DEFAULT_PASSWORD = "password";
+    public static final String DEFAULT_INSTANCE_NAME = "instance";
+
+    private static boolean shutdownHookAdded = false;
+
+    private final String dirName;
+    private final boolean isTempDir;
+    private final String password;
+    private final String instanceName;
+    private final boolean shutdownHook;
+
+    private Path clusterPath;
+    private MiniAccumuloCluster cluster;
+    private Shell shell;
+
+    protected MiniAccumuloClusterWithShellController(final String dirName, final boolean isTempDir, final String password, final String instanceName, final boolean shutdownHook) {
+        this.dirName = dirName;
+        this.isTempDir = isTempDir;
+        this.password = password;
+        this.instanceName = instanceName;
+        this.shutdownHook = shutdownHook;
+    }
+
+    public static void main(final String[] args) {
+        if (null != args && args.length > 4) {
+            System.out.println("Usage: [directory_name] [is_temp_directory] [root_password] [instance_name]");
+        }
+
+        final MiniAccumuloClusterWithShellController runner = new Builder()
+                .dirName(getDirName(args))
+                .tempDir(isTempDir(args))
+                .password(getPassword(args))
+                .instanceName(getInstanceName(args))
+                .shutdownHook(true)
+                .build();
+
+        runner.start();
+        runner.startShell();
+    }
+
+    public void start() {
+        if (shutdownHook) {
+            createShutdownHook();
+        }
+
+        // If it has already been started then do nothing
+        if (null == cluster) {
+            createDirectory();
+            createMiniCluster();
+        }
+    }
+
+    public void startShell() {
+        if (null == cluster) {
+            start();
+        }
+
+        final String[] shellArgs = new String[]{
+                "-u", "root",
+                "-p", cluster.getConfig().getRootPassword(),
+                "-z", cluster.getInstanceName(), cluster.getZooKeepers()};
+
+        try {
+            shell = new Shell();
+            if (!shell.config(shellArgs)) {
+                throw new RuntimeException("Shell failed with code: " + shell.getExitCode());
+            }
+            shell.start();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to start shell");
+        }
+    }
+
+    public void stop() {
+        if (null != shell) {
+            shell.shutdown();
+            shell = null;
+        }
+
+        if (null != cluster) {
+            try {
+                cluster.stop();
+                cluster = null;
+                System.out.println("Cluster stopped");
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (null != clusterPath) {
+            try {
+                FileUtils.deleteDirectory(clusterPath.toFile());
+                clusterPath = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getClusterZooKeepers() {
+        return null != cluster ? cluster.getZooKeepers() : null;
+    }
+
+    private void createShutdownHook() {
+        if (!shutdownHookAdded) {
+            shutdownHookAdded = true;
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    MiniAccumuloClusterWithShellController.this.stop();
+                }
+            });
+        }
+    }
+
+    protected void createMiniCluster() {
+        try {
+            final MiniAccumuloConfig config = new MiniAccumuloConfig(clusterPath.toFile(), password);
+            config.setInstanceName(instanceName);
+            cluster = new MiniAccumuloCluster(config);
+            cluster.start();
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException("Failed to start cluster", e);
+        }
+
+        System.out.println("Cluster started:");
+        System.out.println("\tLocation - " + cluster.getConfig().getDir().getAbsolutePath());
+        System.out.println("\tZookeepers - " + cluster.getZooKeepers());
+        System.out.println("\tInstance name - " + cluster.getInstanceName());
+
+        try {
+            final File propsFile = new File(clusterPath + "/store.properties");
+            FileUtils.copyInputStreamToFile(getClass().getResourceAsStream("/store.properties.template"), propsFile);
+            FileUtils.write(propsFile, "accumulo.zookeepers=" + cluster.getZooKeepers(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createDirectory() {
+        clusterPath = Paths.get(dirName);
+        if (Files.exists(clusterPath)) {
+            try {
+                FileUtils.deleteDirectory(clusterPath.toFile());
+            } catch (IOException e1) {
+                System.err.println("Failed to delete old directory");
+            }
+        }
+
+        try {
+            if (isTempDir) {
+                clusterPath = Files.createTempDirectory(dirName);
+            } else {
+                clusterPath = Files.createDirectory(clusterPath);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to create directory", e);
+        }
+    }
+
+    private static String getDirName(final String[] args) {
+        return getArgOrDefault(0, DEFAULT_DIR_NAME, args);
+    }
+
+    private static boolean isTempDir(final String[] args) {
+        return Boolean.parseBoolean(getArgOrDefault(1, "false", args));
+    }
+
+    private static String getPassword(final String[] args) {
+        return getArgOrDefault(2, DEFAULT_PASSWORD, args);
+    }
+
+    private static String getInstanceName(final String[] args) {
+        return getArgOrDefault(3, DEFAULT_INSTANCE_NAME, args);
+    }
+
+    private static String getArgOrDefault(final int index, final String defaultValue, final String[] args) {
+        return hasArg(index, args) ? args[index] : defaultValue;
+    }
+
+    private static boolean hasArg(final int index, final String[] args) {
+        return null != args && args.length > index && null != args[index];
+    }
+
+    public static class Builder {
+        private String dirName;
+        private boolean isTempDir = false;
+        private String password;
+        private String instanceName;
+        private boolean shutdownHook = true;
+
+        public Builder dirName(final String dirName) {
+            this.dirName = dirName;
+            return this;
+        }
+
+        public Builder tempDir(final boolean tempDir) {
+            this.isTempDir = tempDir;
+            return this;
+        }
+
+        public Builder tempDir() {
+            return tempDir(true);
+        }
+
+        public Builder password(final String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder instanceName(final String instanceName) {
+            this.instanceName = instanceName;
+            return this;
+        }
+
+        public Builder shutdownHook() {
+            return shutdownHook(true);
+        }
+
+        public Builder shutdownHook(final boolean shutdownHook) {
+            this.shutdownHook = shutdownHook;
+            return this;
+        }
+
+        public MiniAccumuloClusterWithShellController build() {
+            return new MiniAccumuloClusterWithShellController(dirName, isTempDir, password, instanceName, shutdownHook);
+        }
+    }
+}
