fac8daebe5624e87840ba461fe6e785a882dd5b5
p013570 <p013570@users.noreply.github.com>
2016-11-30 13:24:06 +0000

    gh-25 - Updated gaffer to 0.4.7

--- python-shell/src/__init__.py
+++ python-shell/src/__init__.py
@@ -2,7 +2,7 @@ from . import gafferpy
 from gafferpy import gaffer_connector
 from gafferpy import gaffer_connector_pki
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 __title__ = "gafferpy"
 __description__ = "Gaffer Python Shell"
