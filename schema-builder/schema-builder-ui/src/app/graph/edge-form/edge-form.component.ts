3e17991e3da8ecd19974b4add94aaf1c1f27a136
l50741 <l50741@users.noreply.github.com>
2017-01-04 10:56:54 +0000

    Edge form now using formBuilder

--- schema-builder/schema-builder-ui/src/app/graph/edge-form/edge-form.component.ts
+++ schema-builder/schema-builder-ui/src/app/graph/edge-form/edge-form.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LocalStorageService } from 'ng2-webstorage';
-import { UUID } from 'angular2-uuid';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-edge-form',
     templateUrl: './edge-form.component.html',
-    styleUrls: ['./edge-form.component.css']
+    styleUrls: ['./edge-form.component.css'],
+    providers: [FormBuilder]
 })
 export class EdgeFormComponent implements OnInit {
     _edge: any;
@@ -14,6 +16,7 @@ export class EdgeFormComponent implements OnInit {
     _network: any;
     _storedTypes: any;
     nodeOptions: any;
+    form: any;
 
     @Input()
     set edges(edges: any) {
@@ -31,6 +34,7 @@ export class EdgeFormComponent implements OnInit {
     @Input()
     set selectedEdge(selectedEdge: any) {
         this._edge = this._edges.get(selectedEdge);
+        this.updateForm(this._edge);
     }
 
     @Input()
@@ -38,20 +42,31 @@ export class EdgeFormComponent implements OnInit {
         this._network = network;
     }
 
-    constructor(private storage: LocalStorageService) { }
+    constructor(private storage: LocalStorageService, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this._storedTypes = this.storage.retrieve('types');
+        this.form.valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .subscribe((data) => this.save(data));
+    }
+
+    updateForm(edge: any) {
+        this.form = this.formBuilder.group({
+            from: edge.from,
+            to: edge.to,
+            label: edge.label,
+            arrows: edge.arrows
+        });
     }
 
     changeEdge(value: any, key: any) {
         this._edge[key] = value;
     }
 
-    save(isValid: boolean, e: any) {
-        if (!isValid) {
-            return;
-        }
+    save(data) {
+        this._edge = _.merge(this._edge, data);
         this._edges.update(this._edge);
         this.storage.store('graphEdges', this._edges);
     }
