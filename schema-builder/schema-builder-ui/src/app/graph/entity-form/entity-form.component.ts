34ca18089c5b9f251c0d06997b9533d674a5393a
l50741 <l50741@users.noreply.github.com>
2017-01-05 10:59:32 +0000

    Update entity form to use observers

--- schema-builder/schema-builder-ui/src/app/graph/entity-form/entity-form.component.ts
+++ schema-builder/schema-builder-ui/src/app/graph/entity-form/entity-form.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LocalStorageService } from 'ng2-webstorage';
+import { FormBuilder } from '@angular/forms';
 import { UUID } from 'angular2-uuid';
+import * as _ from 'lodash';
 
 declare var $: any;
 
 @Component({
     selector: 'app-entity-form',
     templateUrl: './entity-form.component.html',
-    styleUrls: ['./entity-form.component.css']
+    styleUrls: ['./entity-form.component.css'],
+    providers: [FormBuilder]
 })
 export class EntityFormComponent implements OnInit {
     _node: any;
     _nodes: any;
     _storedTypes: any;
     entities: any;
+    form: any;
 
     @Input()
     set nodes(nodes: any) {
@@ -25,12 +29,25 @@ export class EntityFormComponent implements OnInit {
     set selectedNode(selectedNode: any) {
         this._node = this._nodes.get(selectedNode);
         this.entities = this._node.entities || [];
+        this.updateForm(this.entities);
     }
 
-    constructor(private storage: LocalStorageService) { }
+    constructor(private storage: LocalStorageService, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this._storedTypes = this.storage.retrieve('types');
+    }
+
+    updateForm(entities: any) {
+        let formObject = {};
+        _.forEach(entities, (entity: any) => {
+            formObject[entity.id] = entity.name;
+        });
+        this.form = this.formBuilder.group(formObject);
+        this.form.valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .subscribe((data) => this.save(data));
     }
 
     addNewEntity() {
@@ -40,9 +57,20 @@ export class EntityFormComponent implements OnInit {
             name: 'New Entity',
             properties: []
         });
+        this.updateForm(this.entities);
     }
 
-    save() {
+    removeEntity(entityId) {
+        this.entities = _.filter(this.entities, (entity: any) => {
+            return entity.id !== entityId;
+        });
+        this.updateForm(this.entities);
+    }
+
+    save(data) {
+        _.forEach(this.entities, (entity: any) => {
+            entity.name = data[entity.id];
+        });
         this._node.entities = this.entities;
         this._nodes.update(this._node);
         this.storage.store('graphNodes', this._nodes);
