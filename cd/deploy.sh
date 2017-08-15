e9d76fa01e38833cc2abe7e0ac640d0d2e5ff17d
p013570 <p013570@users.noreply.github.com>
2017-08-15 13:20:36 +0100

    Updated travis scripts to release modules separately

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -3,5 +3,5 @@
 if [ "$MODULES" == '' ]; then
     mvn -q deploy -P sign,build-extras,quick --settings cd/mvnsettings.xml -B
 else
-    mvn -q deploy -P sign,build-extras,quick --settings cd/mvnsettings.xml -B -pl $MODULES
+    mvn -q deploy -P sign,build-extras,quick --settings cd/mvnsettings.xml -B -pl $MODULES -am
 fi
