c1dd8cddda3bda85dbcddfb2e1269549f9b4222b
p013570 <p013570@users.noreply.github.com>
2017-09-26 06:05:37 +0100

    gh-154 - updated travis scripts with 'set -e'

--- cd/verify.sh
+++ cd/verify.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$TRAVIS_BRANCH" != 'master' ] || [ "$TRAVIS_PULL_REQUEST" == 'true' ]; then
     if [ "$MODULES" == '' ]; then
         echo "Running verify script: mvn -q verify -P travis,analyze -B"
