83a1019ecd894606724db22511bc7ca779254d7e
d47853 <d47853@users.noreply.github.com>
2017-10-12 08:58:44 +0100

    gh-200 fixed raw view

--- ui/src/main/webapp/app/config/icon-config.js
+++ ui/src/main/webapp/app/config/icon-config.js
@@ -4,4 +4,5 @@ angular.module('app').config(['$mdIconProvider', function($mdIconProvider) {
 
     $mdIconProvider
         .icon('logo', 'app/img/logo.svg')
+        .icon('save', 'app/img/save.svg')
 }])
