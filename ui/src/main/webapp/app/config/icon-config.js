b8cb5d830a010629a04b1057f53221e38d168df7
d47853 <d47853@users.noreply.github.com>
2017-10-18 11:28:40 +0100

    gh-234 fixed first page of query builder and fixed height issue on table view

--- ui/src/main/webapp/app/config/icon-config.js
+++ ui/src/main/webapp/app/config/icon-config.js
@@ -5,4 +5,5 @@ angular.module('app').config(['$mdIconProvider', function($mdIconProvider) {
     $mdIconProvider
         .icon('logo', 'app/img/logo.svg')
         .icon('save', 'app/img/save.svg')
+        .icon('info', 'app/img/info.svg')
 }])
