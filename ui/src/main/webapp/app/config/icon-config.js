dfd3be220190bc053eaf2dacf52b42ad0f9f4176
d47853 <d47853@users.noreply.github.com>
2017-10-19 16:26:07 +0100

    gh-234 removed logo

--- ui/src/main/webapp/app/config/icon-config.js
+++ ui/src/main/webapp/app/config/icon-config.js
@@ -19,7 +19,6 @@
 angular.module('app').config(['$mdIconProvider', function($mdIconProvider) {
 
     $mdIconProvider
-        .icon('logo', 'app/img/logo.svg')
         .icon('save', 'app/img/save.svg')
         .icon('info', 'app/img/info.svg');
 }]);
