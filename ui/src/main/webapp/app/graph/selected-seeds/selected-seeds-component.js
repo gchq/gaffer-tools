62bd9c413a286c7944b1fdd92d28263d197dbbb1
d47853 <d47853@users.noreply.github.com>
2017-11-24 12:27:23 +0000

    gh-314 split graph page into two components. Surrounded graph and selected elements in cards

---
+++ ui/src/main/webapp/app/graph/selected-seeds/selected-seeds-component.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+angular.module('app').component('selectedSeeds', selectedSeeds());
+
+function selectedSeeds() {
+    return {
+        templateUrl: 'app/graph/selected-seeds/selected-seeds.html',
+        controller: SelectedSeedsController,
+        controllerAs: 'ctrl'
+    }
+}
+
+function SelectedSeedsController($scope, $timeout, events, graph, types, schema) {
+    var vm = this;
+
+    vm.selectedEdges = graph.getSelectedEdges();
+    vm.selectedEntities = graph.getSelectedEntities();
+    vm.schema;
+
+    var promise;
+
+    schema.get().then(function(schema) {
+        vm.schema = schema;
+    });
+
+    events.subscribe('selectedElementsUpdate', function(selectedElements) {
+        vm.selectedEdges = selectedElements.edges;
+        vm.selectedEntities = selectedElements.entities;
+
+        if(!promise) {
+            promise = $timeout(function() {
+                $scope.$apply();
+                promise = null;
+            })
+        }
+    });
+
+    vm.resolve = function(value) {
+        return types.getShortValue(value);
+    }
+
+    vm.resolveVertex = function(value) {
+        return types.getShortValue(JSON.parse(value));
+    }
+}
