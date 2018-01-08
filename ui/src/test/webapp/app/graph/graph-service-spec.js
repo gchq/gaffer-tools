88c42d098c5a556526ee988fc0424164a0a8eb11
d47853 <d47853@users.noreply.github.com>
2018-01-08 13:07:38 +0000

    gh-312 added new layout to pom and corrected graph.load test to account for new layout

--- ui/src/test/webapp/app/graph/graph-service-spec.js
+++ ui/src/test/webapp/app/graph/graph-service-spec.js
@@ -1,7 +1,8 @@
 describe("The Graph Service", function() {
 
     var graph;
-    var events
+    var events;
+    var scope;
 
     beforeEach(module('app'));
 
@@ -25,64 +26,68 @@ describe("The Graph Service", function() {
         });
     }));
 
-    beforeEach(inject(function(_graph_, _events_) {
+    beforeEach(inject(function(_graph_, _events_, _$rootScope_) {
         graph = _graph_;
         events = _events_;
+        scope = _$rootScope_.$new();
     }));
-
-    beforeEach(function() {
-        graph.load(); // simulating the call performed when MainCtrl starts
-    });
 
     describe('when loading', function() {
 
-        var scope;
+        var resolvedValue;
 
-        beforeEach(inject(function(_$rootScope_) {
-            var $rootScope = _$rootScope_;
-
-            scope = $rootScope.$new();
-        }));
-
-
-        it('should load cytoscape and return the cytoscape graph object', function(done) {
+        beforeEach(function(done) {
             graph.load().then(function(graphObject) {
-                expect(graphObject).toBeDefined();
+                resolvedValue = graphObject;
                 done();
             });
 
-            scope.$digest();
+            setTimeout(function() {
+                scope.$apply();
+            }, 1000)
+        });
+
+        it('should load cytoscape and return the cytoscape graph object', function() {
+            expect(resolvedValue).toBeDefined();
         });
     });
 
-    describe('when adding a seed', function() {
-        it('should also select it', function() {
-            graph.addSeed("mySeed");
-            expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]})
+    describe('after loading', function() {
+        beforeEach(function() {
+            graph.load(); // simulating the call performed when MainCtrl starts
         });
 
-        it('should broadcast the selectedElementsUpdate event', function() {
-            spyOn(events, 'broadcast');
-            graph.addSeed('mySeed');
-            expect(events.broadcast).toHaveBeenCalledTimes(1);
-            expect(events.broadcast.calls.first().args[1]).toEqual([{entities: { '"mySeed"': [{vertex: '"mySeed"'}]}, edges: {}}]);
-        });
+        describe('when adding a seed', function() {
+            it('should also select it', function() {
+                graph.addSeed("mySeed");
+                expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]})
+            });
 
-        it('should select it if already added', function() {
-            // add it the first time
-            graph.addSeed("mySeed");
-            // deselect it
-            graph.reset();
+            it('should broadcast the selectedElementsUpdate event', function() {
+                spyOn(events, 'broadcast');
+                graph.addSeed('mySeed');
+                expect(events.broadcast).toHaveBeenCalledTimes(1);
+                expect(events.broadcast.calls.first().args[1]).toEqual([{entities: { '"mySeed"': [{vertex: '"mySeed"'}]}, edges: {}}]);
+            });
 
-            graph.addSeed("mySeed");
-            expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]})
-        });
+            it('should select it if already added', function() {
+                // add it the first time
+                graph.addSeed("mySeed");
+                // deselect it
+                graph.reset();
 
-        it('should do nothing if already added and selected', function() {
-            graph.addSeed("mySeed");
-            expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]});
-            graph.addSeed("mySeed");
-            expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]});
+                graph.addSeed("mySeed");
+                expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]})
+            });
+
+            it('should do nothing if already added and selected', function() {
+                graph.addSeed("mySeed");
+                expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]});
+                graph.addSeed("mySeed");
+                expect(graph.getSelectedEntities()).toEqual({'"mySeed"': [{vertex: '"mySeed"'}]});
+            });
         });
-    });
+    })
+
+
 })
