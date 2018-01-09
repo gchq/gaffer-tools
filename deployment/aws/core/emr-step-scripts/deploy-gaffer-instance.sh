93cbedf8781d633c2b8602091e417bc42d844977
ctas582 <ctas582@users.noreply.github.com>
2018-01-09 11:07:22 +0000

    gh-354 Refactoring scripts so that they are downloaded from Github, rather than generated from inside the deployment script

--- deployment/aws/core/emr-step-scripts/deploy-gaffer-instance.sh
+++ deployment/aws/core/emr-step-scripts/deploy-gaffer-instance.sh
@@ -134,11 +134,13 @@ cd accumulo-pkg
 
 ACCUMULO_PKG_BUILD_URL1=https://raw.githubusercontent.com/gchq/gaffer-tools/gaffer-tools-$GAFFER_TOOLS_VERSION/slider/scripts/build_accumulo_package.sh
 ACCUMULO_PKG_BUILD_URL2=https://raw.githubusercontent.com/gchq/gaffer-tools/$GAFFER_TOOLS_VERSION/slider/scripts/build_accumulo_package.sh
+TOOLS_GITHUB_ROOT_URL="https://raw.githubusercontent.com/gchq/gaffer-tools/gaffer-tools-$GAFFER_TOOLS_VERSION"
 
 echo "Trying to download Accumulo build script from $ACCUMULO_PKG_BUILD_URL1"
 if ! curl -fLO $ACCUMULO_PKG_BUILD_URL1; then
 	echo "Trying to download Accumulo build script from $ACCUMULO_PKG_BUILD_URL2"
 	curl -fLO $ACCUMULO_PKG_BUILD_URL2
+	TOOLS_GITHUB_ROOT_URL="https://raw.githubusercontent.com/gchq/gaffer-tools/$GAFFER_TOOLS_VERSION"
 fi
 
 if [ ! -f ./build_accumulo_package.sh ]; then
@@ -279,199 +281,12 @@ echo "$GAFFER_SLIDER_POM_VERSION" >gaffer-slider.version
 echo "$ROOT_PWD" >root.password
 echo "$SECRET" >instance.secret
 
-echo "Generating admin helper scripts..."
+echo "Downloading utility scripts..."
 cd $DST
-
-tee install-accumulo-client.sh <<EOF
-#!/bin/bash -e
-
-DIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
-cd \$DIR
-
-DST=accumulo
-CLUSTER_NAME=\$(cat ./etc/cluster.name)
-ACCUMULO_VERSION=\$(cat ./etc/accumulo.version)
-
-if [ ! -d "\$DST" ]; then
-	mkdir -p "\$DST"
-	./slider client \\
-		--install \\
-		--dest "\$DST" \\
-		--package ./accumulo-pkg/slider-accumulo-app-package-\$ACCUMULO_VERSION.zip \\
-		--name \$CLUSTER_NAME \\
-		--debug \\
-	|| (rm -rf "\$DST" && exit 1)
-fi
-
-EOF
-
-tee accumulo-shell.sh <<EOF
-#!/bin/bash -e
-
-DIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
-cd \$DIR
-./install-accumulo-client.sh || exit 1
-./accumulo/*/bin/accumulo shell -u root -p file:./etc/root.password --disable-auth-timeout "\$@"
-
-EOF
-
-tee create-accumulo-user.sh <<EOF
-#!/bin/bash -e
-
-USERNAME=""
-PASSWORD=""
-VISIBILITIES=""
-
-while [[ \$# -gt 0 ]]; do
-	key="\$1"
-
-	case \$key in
-		-p|--password)
-			PASSWORD=\$2
-			shift
-			;;
-		-v|--visibilities)
-			VISIBILITIES=\$2
-			shift
-			;;
-		*)
-			USERNAME=\$1
-			;;
-	esac
-	shift
-done
-
-if [ "\$USERNAME" == "" ]; then
-	echo "Usage: \$0 <username> [-v <visibilities>]"
-	exit 1
-fi
-
-if [ "\$PASSWORD" == "" ]; then
-	while true; do
-		read -s -p "Password: " PASSWORD
-		echo
-		read -s -p "Confirm Password: " PASSWORD2
-		echo
-		[ "\$PASSWORD" = "\$PASSWORD2" ] && break
-		echo "Passwords do not match, please try again..."
-	done
-fi
-
-DIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
-cd \$DIR
-./install-accumulo-client.sh || exit 1
-
-./accumulo/*/bin/accumulo shell -u root -p file:./etc/root.password -e "createuser \$USERNAME" <<ARGS
-\$PASSWORD
-\$PASSWORD
-ARGS
-
-./accumulo/*/bin/accumulo shell -u root -p file:./etc/root.password -e "grant -u \$USERNAME -s System.CREATE_TABLE"
-
-if [[ "\$VISIBILITIES" != "" ]]; then
-	./accumulo/*/bin/accumulo shell -u root -p file:./etc/root.password -e "setauths -u \$USERNAME -s \$VISIBILITIES"
-fi
-
-EOF
-
-tee create-accumulo-user-with-kms.sh <<EOF
-#!/bin/bash -e
-
-USERNAME=""
-VISIBILITIES=""
-KMS_ID=""
-PARAM_NAME=""
-WAIT_HANDLE_URL=""
-
-while [[ \$# -gt 0 ]]; do
-	key="\$1"
-
-	case \$key in
-		-k|--kms)
-			KMS_ID=\$2
-			shift
-			;;
-		-p|--param)
-			PARAM_NAME=\$2
-			shift
-			;;
-		-v|--visibilities)
-			if [ "\$2" != "none" ]; then
-				VISIBILITIES=\$2
-			fi
-			shift
-			;;
-		-w|--wait-handle-url)
-			WAIT_HANDLE_URL=\$2
-			shift
-			;;
-		*)
-			USERNAME=\$1
-			;;
-	esac
-	shift
-done
-
-if [[ "\$WAIT_HANDLE_URL" ]]; then
-	function awsSignal {
-		/opt/aws/bin/cfn-signal -e \$? "\$WAIT_HANDLE_URL"
-	}
-	trap awsSignal EXIT
-fi
-
-function retry {
-	local cmd="\$@"
-	local rc=-1
-	local n=1
-	local limit=60
-
-	set +e
-	until [[ \$rc -eq 0 || \$n -gt \$limit ]]; do
-		let n=n+1
-		sleep 10
-		eval \$cmd
-		rc=\$?
-	done
-
-	set -e
-	return \$rc
-}
-
-if [[ "\$USERNAME" == "" || "\$KMS_ID" == "" || "\$PARAM_NAME" == "" ]]; then
-	echo "Usage: \$0 <username> -k <kmsID> -p <ssmParameterName> [-v <visibilities>] [-w <WaitHandleUrl>]"
-	exit 1
-fi
-
-# Generate password for Accumulo
-PASSWORD=\$(openssl rand -base64 32)
-
-# Encrypt password using AWS KMS
-ENCRYPTED_PASSWORD=\$(aws kms encrypt --region "\$AWS_DEFAULT_REGION" --key-id "\$KMS_ID" --plaintext "\$PASSWORD" --output text --query CiphertextBlob)
-if [ "\$ENCRYPTED_PASSWORD" == "" ]; then
-	echo "Unable to use AWS KMS: \$KMS_ID to encrypt password!"
-	exit 1
-fi
-
-# Put encrypted password into a SSM Parameter based Secret Store
-# Since emr-5.7.0 this next command has started to fail as AWS is eventually consistent so sometimes this command runs
-# before the IAM policy that gives the EMR cluster access to the SSM Parameter has been applied, which causes an error
-# and this script to fail. We need to retry it for a few minutes.
-# @see https://stackoverflow.com/questions/20156043/how-long-should-i-wait-after-applying-an-aws-iam-policy-before-it-is-valid
-retry aws ssm put-parameter --name "\$PARAM_NAME" --value "\$ENCRYPTED_PASSWORD" --type String --overwrite
-
-DIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
-cd \$DIR
-
-if [[ "\$VISIBILITIES" != "" ]]; then
-	VISIBILITIES="-v \$VISIBILITIES"
-fi
-
-./create-accumulo-user.sh \$USERNAME \$VISIBILITIES <<ARGS
-\$PASSWORD
-\$PASSWORD
-ARGS
-
-EOF
+curl -fLO $TOOLS_GITHUB_ROOT_URL/deployment/aws/core/utility-scripts/install-accumulo-client.sh
+curl -fLO $TOOLS_GITHUB_ROOT_URL/deployment/aws/core/utility-scripts/accumulo-shell.sh
+curl -fLO $TOOLS_GITHUB_ROOT_URL/deployment/aws/core/utility-scripts/create-accumulo-user.sh
+curl -fLO $TOOLS_GITHUB_ROOT_URL/deployment/aws/core/utility-scripts/create-accumulo-user-with-kms.sh
 
 chmod +x *.sh
 
