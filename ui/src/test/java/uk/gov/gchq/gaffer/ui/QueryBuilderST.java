816f65c82844e8d2a964a6793e61b2e9963f1d83
d47853 <d47853@users.noreply.github.com>
2018-01-18 13:51:57 +0000

    gh-341 fixed merge error in system test

--- ui/src/test/java/uk/gov/gchq/gaffer/ui/QueryBuilderST.java
+++ ui/src/test/java/uk/gov/gchq/gaffer/ui/QueryBuilderST.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
