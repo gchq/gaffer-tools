25aafb98dea8098d3fc39171d126e0e61b62d32d
p013570 <p013570@users.noreply.github.com>
2018-03-13 15:27:54 +0000

    Updated copyright dates

--- slider/package/scripts/gaffer.py
+++ slider/package/scripts/gaffer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright 2017 Crown Copyright
+# Copyright 2017-2018 Crown Copyright
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
