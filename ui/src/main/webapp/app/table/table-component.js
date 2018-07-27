282e02f4527228036bd31bc6929fdfdb914c3392
d47853 <d47853@users.noreply.github.com>
2018-07-27 17:51:04 +0100

    gh-559 rewrote tests

--- ui/src/main/webapp/app/table/table-component.js
+++ ui/src/main/webapp/app/table/table-component.js
@@ -36,7 +36,7 @@ function resultsTable() {
  * @param {*} types For converting objects based on their types
  * @param {*} time For converting time objects
  */
-function TableController(schema, results, table, events, common, types, time, csv) {
+function TableController(schema, results, table, events, common, types, time, csv, $q) {
     var vm = this;
     var resultsByType = [];
     vm.filteredResults = [];
