e343e0c5865afdbffd571ab3fd14c1e1e54acc56
p013570 <p013570@users.noreply.github.com>
2018-07-30 09:36:40 +0100

    gh-559 - separated download data into separate method

--- ui/src/main/webapp/app/table/table-component.js
+++ ui/src/main/webapp/app/table/table-component.js
@@ -119,7 +119,7 @@ function TableController(schema, results, table, events, common, types, time, cs
         processOtherTypes(ids, properties, resultsData);
 
         vm.data.allColumns = common.concatUniqueValues(common.concatUniqueValues(ids, groupByProperties), properties);
-        
+
         if (!vm.data.columns || vm.data.columns.length === 0) {
             vm.data.columns = angular.copy(vm.data.allColumns);
         }
@@ -259,7 +259,10 @@ function TableController(schema, results, table, events, common, types, time, cs
         var mimeType = 'data:text/csv;charset=utf-8';
         var data = csv.generate(vm.filteredResults, vm.data.columns);
         var fileName = 'gaffer_results_' + Date.now() + '.csv'
+        downloadData(fileName, data, mimeType);
+    }
 
+    var downloadData = function(fileName, data, mimeType) {
         var downloadLink = document.createElement('a');
         downloadLink.href = URL.createObjectURL(new Blob([data], {type: mimeType}));
         downloadLink.download = fileName;
