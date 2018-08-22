593f340c4024a02dfe9173b6e6b9262cc230a52a
d47853 <d47853@users.noreply.github.com>
2018-08-22 07:13:36 +0100

    gh-473 fixed bug where table columns wouldn't reload after quick query

--- ui/src/main/webapp/app/table/table-component.js
+++ ui/src/main/webapp/app/table/table-component.js
@@ -103,7 +103,9 @@ function TableController(schema, results, table, events, common, types, time, cs
     }
 
     var onResultsUpdated = function(res) {
-        table.setCachedValues({});
+        // forcing a cache reload ensures columns are recalculated if they need to be
+        cacheValues();
+        loadFromCache();
         processResults(res);
     }
 
@@ -132,7 +134,7 @@ function TableController(schema, results, table, events, common, types, time, cs
             }
         }
 
-        if (!vm.data.types || vm.data.columns.length === 0) {
+        if (!vm.data.types || vm.data.types.length === 0) {
             vm.data.types = angular.copy(vm.data.allTypes);
         }
         if (!vm.data.groups || vm.data.groups.length === 0) {
@@ -288,15 +290,9 @@ function TableController(schema, results, table, events, common, types, time, cs
         var cachedValues = table.getCachedValues();
         vm.searchTerm = cachedValues.searchTerm;
         vm.sortType =  cachedValues.sortType;
-        if(cachedValues.columns && cachedValues.columns.length > 0) {
-            vm.data.columns = cachedValues.columns;
-        }
-        if(cachedValues.types && cachedValues.types.length > 0) {
-            vm.data.types = cachedValues.types;
-        }
-        if(cachedValues.groups && cachedValues.groups.length > 0) {
-            vm.data.groups = cachedValues.groups;
-        }
+        vm.data.columns = cachedValues.columns;
+        vm.data.types = cachedValues.types;
+        vm.data.groups = cachedValues.groups;
     }
 
     var cacheValues = function() {
