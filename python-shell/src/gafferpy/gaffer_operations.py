2e60c8004b80f6231d7007b53d66cc5850516cd4
m55624 <29117199+m55624@users.noreply.github.com>
2018-09-20 06:50:52 +0100

    gh-582 - added Reduce op and test (#586)
    * gh-582 - added Reduce op and test
    
    * gh-582 - indentation fix
    
    * gh-582 updated test changing max op to binary operator
    
    * gh-582 added quote
    
    * gh-582 corrected binary operator
    
    * gh-571 manual merge
    
    * gh-582 renamed aggregateFunction to aggregate_function
    
    * gh-582 reduced duplicated operation and corrected conditional
    
    * gh-582 - fixed reduce operation

--- python-shell/src/gafferpy/gaffer_operations.py
+++ python-shell/src/gafferpy/gaffer_operations.py
@@ -2397,6 +2397,54 @@ class While(Operation):
         return operation
 
 
+class Reduce(Operation):
+    CLASS = 'uk.gov.gchq.gaffer.operation.impl.Reduce'
+
+    def __init__(self, input=None, identity=None,
+                 aggregation_function=None, options=None):
+
+        super().__init__(_class_name=self.CLASS,
+                         options=options)
+
+        self.input = input
+        self.identity = identity
+
+        if aggregation_function is None:
+            raise ValueError('aggregation_function is required')
+        if isinstance(aggregation_function, dict):
+            aggregation_function = JsonConverter.from_json(
+                aggregation_function, gaffer_binaryoperators.BinaryOperator)
+        self.aggregation_function = aggregation_function
+
+    def to_json(self):
+        operation = super().to_json()
+
+        if self.input is not None:
+            json_seeds = []
+            if isinstance(self.input, list):
+                for seed in self.input:
+                    if isinstance(seed, ToJson):
+                        json_seeds.append(seed.to_json())
+                    else:
+                        json_seeds.append(seed)
+            else:
+                if isinstance(self.input, ToJson):
+                    json_seeds.append(self.input.to_json())
+                else:
+                    json_seeds.append(self.input.to_json())
+            operation['input'] = json_seeds
+
+        operation['aggregationFunction'] = self.aggregation_function.to_json()
+
+        if self.identity is not None:
+            if isinstance(self.identity, ToJson):
+                operation['identity'] = self.identity.to_json()
+            else:
+                operation['identity'] = self.identity
+
+        return operation
+
+
 class ForEach(Operation):
     CLASS = 'uk.gov.gchq.gaffer.operation.impl.ForEach'
 
