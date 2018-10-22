3ca1ed2477f49ab8f165915debdacfa04aa39e97
d47853 <d47853@users.noreply.github.com>
2018-10-22 16:54:53 +0100

    gh-574 added tests for graph service

--- ui/src/test/webapp/app/graph/graph-service-spec.js
+++ ui/src/test/webapp/app/graph/graph-service-spec.js
@@ -1,62 +1,119 @@
-// describe('The Graph Service', function() {
+describe('The Graph service', function() {
 
-//     beforeEach(module('app'));
+    var service;
 
-//     beforeEach(module(function($provide) {
-//         $provide.factory('config', function($q) {
-//             var get = function() {
-//                 return $q.when({});
-//             }
-
-//             return {
-//                 get: get
-//             }
-//         });
-
-//         $provide.factory('schema', function($q) {
-//             return {
-//                 get: function() {
-//                     return $q.when({});
-//                 }
-//             }
-//         });
-//     }));
-
-//     describe('The Controller', function() {
-//         var $componentController, $timeout, $q;
-//         var graph, scope;
+    beforeEach(module('app'));
 
 
-//         beforeEach(inject(function(_$componentController_, _graph_, _$rootScope_, _$timeout_, _$q_) {
-//             $componentController = _$componentController_;
-//             graph = _graph_;
-//             var $rootScope = _$rootScope_;
-//             scope = $rootScope.$new();
-//             $timeout = _$timeout_;
-//             $q = _$q_;
-//         }));
+    beforeEach(inject(function(_graph_) {
+        service = _graph_;
+    }));
 
-//         it('should exist', function() {
-//             var ctrl = $componentController('graph');
-//             expect(ctrl).toBeDefined();
-//         });
+    describe('graph.getGraphConfiguration()', function() {
+        it('should pass the value of the configuration so that mutation of the service value cannot occur outside the service', function() {
+            service.setGraphConfiguration({ 'test': true });
+            var conf = service.getGraphConfiguration();
+            conf['foo'] = 'bar';
 
-//         it('should load the graph on startup', function() {
-//             spyOn(graph, 'load').and.returnValue($q.when());
-//             spyOn(graph, 'reset');
-//             var ctrl = $componentController('graph', {$scope: scope});
-//             ctrl.$onInit();
-//             $timeout.flush();
-//             expect(graph.load).toHaveBeenCalledTimes(1);
-//         });
+            expect(service.getGraphConfiguration().foo).toBeUndefined();
+        })
+    });
 
-//         it('should reset the graph elements on startup', function() {
-//             spyOn(graph, 'load').and.returnValue($q.when());
-//             spyOn(graph, 'reset');
-//             var ctrl = $componentController('graph', {$scope: scope});
-//             ctrl.$onInit();
-//             $timeout.flush();
-//             expect(graph.reset).toHaveBeenCalledTimes(1);
-//         });
-//     });
-// });
+    describe('graph.setGraphConfiguration()', function() {
+        it('should pass a value to the service object ensuring that the service configuration cannot be mutated outside the service', function() {
+            var test = {
+                'foo': 'bar'
+            }
+
+            service.setGraphConfiguration(test);
+
+            test.isPassed = true;
+
+            expect(service.getGraphConfiguration()).toEqual({
+                'foo': 'bar'
+            });
+        });
+    });
+
+    describe('graph.getSelectedElements()', function() {
+        it('should pass the value of the selected elements so that mutation of the service value cannot occur outside the service', function() {
+            service.setSelectedElements({ 'test': true });
+            var conf = service.getSelectedElements();
+            conf['foo'] = 'bar';
+
+            expect(service.getSelectedElements().foo).toBeUndefined();
+        })
+    });
+
+    describe('graph.setSelectedElements()', function() {
+        it('should pass a value to the service object ensuring that the selected elements in the service cannot be mutated outside the service', function() {
+            var test = {
+                'foo': 'bar'
+            }
+
+            service.setSelectedElements(test);
+
+            test.isPassed = true;
+
+            expect(service.getSelectedElements()).toEqual({
+                'foo': 'bar'
+            });
+        });
+    });
+
+    describe('graph.getSearchTerm()', function() {
+        it('should pass the value of the search term so that mutation of the service value cannot occur outside the service', function() {
+            service.setSearchTerm('test');
+            var st = service.getSearchTerm();
+            st += 'case';
+
+            expect(service.getSearchTerm()).toEqual('test');
+        })
+    });
+
+    describe('graph.setSearchTerm()', function() {
+        it('should pass a value to the service object ensuring that the service configuration cannot be mutated outside the service', function() {
+            var test = 'test'
+
+            service.setSearchTerm(test);
+
+            test = 'new test';
+
+            expect(service.getSearchTerm()).toEqual('test');
+        });
+    });
+
+    describe('graph.deselectAll()', function() {
+
+        beforeEach(function() {
+            service.setSelectedElements({
+                entities: {
+                    'test': [],
+                    'test2': [ {
+                        group: 'g',
+                        properties: {}
+                    }]
+                },
+                edges: {
+                    'myEdgeid': [{
+                        source: 's',
+                        destination: 'd',
+                        directed: false,
+                        properties: {}
+                    }]
+                }
+            });
+        });
+
+        it('should reset the selected Entities and Edges to empty objects', function() {
+            service.deselectAll();
+
+            var expected = {
+                entities: {},
+                edges: {}
+            }
+
+            expect(service.getSelectedElements()).toEqual(expected);
+        });
+    });
+});
