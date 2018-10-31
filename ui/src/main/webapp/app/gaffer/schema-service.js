5363798b9c9bb6f10ce33844eaa0aed243ccbd2d
d47853 <d47853@users.noreply.github.com>
2018-10-31 10:20:22 +0000

    gh-572 added documentation to methods

--- ui/src/main/webapp/app/gaffer/schema-service.js
+++ ui/src/main/webapp/app/gaffer/schema-service.js
@@ -25,6 +25,12 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
     var schemaVertices = {};
     
 
+    /**
+     * Asynchronously gets the schema. It will reject the promise if it fails to get the schema but won't
+     * broadcast an error. The schema will be saved until update is called to reduce number of http requests. 
+     * If called while an existing request is in progress, it will be resolved by the existing request, 
+     * rather than sending another one.
+     */
     service.get = function() {
         if (schema) {
             return $q.when(schema);
@@ -35,6 +41,10 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         return deferred.promise;
     }
 
+    /**
+     * Creates the get schema operation using the default operation options.
+     * @param {Boolean} loud Flag passed down to indicate whether to broadcast errors
+     */
     var getSchema = function(loud) {
         var getSchemaOperation = operationService.createGetSchemaOperation();
         if (Object.keys(getSchemaOperation.options).length === 0) {
@@ -47,6 +57,12 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         }
     }
 
+    /**
+     * Runs the GetSchema operation. Will fail if the Request sends back a non-200 response or the query.execute method
+     * errors
+     * @param {Operation} operation The GetSchema operation
+     * @param {*} loud A flag indicating whether to broadcast errors
+     */
     var getSchemaWithOperation = function(operation, loud) {
         try {
             query.execute(
@@ -84,6 +100,11 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         }
     }
 
+    /**
+     * Updates the schema service
+     * Rejects all current promises if outstanding. Then loudly loads the schema.
+     * Once finished, the schema or error is returned.
+     */
     service.update = function() {
         if (deferred) {
             deferred.reject('Reloading the schema');
@@ -93,10 +114,16 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         return deferred.promise;
     }
 
+    /**
+     * Returns the schema vertices.
+     */
     service.getSchemaVertices = function() {
         return schemaVertices;
     }
 
+    /**
+     * Function which updates the schema vertices.
+     */
     var updateSchemaVertices = function() {
         var vertices = [];
         if(schema) {
@@ -118,6 +145,26 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         schemaVertices = vertices;
     }
 
+    /**
+     * Returns an object representing the source and destination of the edge group. The object returned holds the 
+     * vertex types and schema definition for those vertex types. It looks something like this:
+     * 
+     *  {
+     *      "source": {
+     *          "sourceVertexType": {
+     *              "class": "sourceVertexClass",
+     *              "serialiser": "vertexSerialiserClass"
+     *              ...
+     *          }        
+     *      },
+     *      "destination": {
+     *          "destinationVertexType": {
+     *              "class": "destinationVertexClass"
+     *              ...
+     *          }
+     *      }
+     *  }
+     */
     service.getVertexTypesFromEdgeGroup = function(group) {
         if (!schema || !schema.edges[group]) {
             return {source: null, destination: null};
@@ -134,6 +181,11 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         return vertexTypes;
     }
 
+    /**
+     * Returns an object which holds a key (the vertex type) and value 
+     * (the schema type definition for the vertex type). It returns null if the schema doesn't exist or
+     * The Entity group does not exist in the schema.
+     */
     service.getVertexTypeFromEntityGroup = function(group) {
         if (!schema || !schema.entities[group]) {
             return null;
@@ -147,6 +199,10 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         return vertexType;
     }
 
+    /**
+     * Gets the property object for a given entity group. Returns undefined if
+     * the entity group contains no properties.
+     */
     service.getEntityProperties = function(entity) {
         if(Object.keys(schema.entities[entity].properties).length) {
             return schema.entities[entity].properties;
@@ -154,6 +210,10 @@ angular.module('app').factory('schema', ['$http', 'config', '$q', 'common', 'ope
         return undefined;
     }
 
+    /**
+     * Gets the property object for a given edge group. Returns undefined if the
+     * edge group contains no properties.
+     */
     service.getEdgeProperties = function(edge) {
         if(Object.keys(schema.edges[edge].properties).length) {
             return schema.edges[edge].properties;
