a642d61758b3cf4f25ff72340ca792536bd6442f
Guy Matthews <g609bmsma@protonmail.com>
2019-03-07 11:51:11 +0000

    Using "\u008" as backspace

--- ui/src/test/java/uk/gov/gchq/gaffer/ui/QueryBuilderST.java
+++ ui/src/test/java/uk/gov/gchq/gaffer/ui/QueryBuilderST.java
@@ -243,8 +243,7 @@ public class QueryBuilderST {
         autoComplete("operation-name", "Two Hop With Limit");
         enterText("seedVertices", "M5");
         click("param1-");
-        backspace("param1-");
-        enterText("param1-", "2");
+        enterText("param1-", "\u008", "2");
         click("execute-chain");
         click("open-raw");
         clickTab("Results");
@@ -262,10 +261,6 @@ public class QueryBuilderST {
 
     private void enterText(final String id, final String value) {
         getElement(id).sendKeys(value);
-    }
-
-    private void backspace (final String id) {
-        getElement(id).sendKeys(Keys.backspace);
     }
 
     private void autoComplete(final String id, final String input) throws InterruptedException {
