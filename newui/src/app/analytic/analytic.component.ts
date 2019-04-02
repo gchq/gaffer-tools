9300d1e1a00de58d4adf795a37f2f8ba3b10ccbd
Guy Matthews <g609bmsma@protonmail.com>
2019-04-02 14:27:23 +0100

    analytic component converted to angular

--- newui/src/app/analytic/analytic.component.ts
+++ newui/src/app/analytic/analytic.component.ts
@@ -6,29 +6,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./analytic.component.css"]
 })
 export class AnalyticComponent implements OnInit {
-  constructor() {
-    function analytic() {
-      return {
-        controller: AnalyticController,
-        controllerAs: "ctrl",
-        bindings: {
-          model: "<"
-        }
-      };
-    }
+  navigation;
+  operationChain;
 
-    function AnalyticController(navigation, operationChain) {
-      var vm = this;
-
-      vm.$onInit = function() {};
-
-      // Save the chosen analytic in the operationChain service (should change to use analyticsService)
-      vm.execute = function(operation) {
-        operationChain.setOperation(operation);
-        navigation.goTo("parameters");
-      };
-    }
+  // Save the chosen analytic in the operationChain service (should change to use analyticsService)
+  execute(operation) {
+    this.operationChain.setOperation(operation);
+    this.navigation.goTo("parameters");
   }
+
+  constructor() {}
 
   ngOnInit() {}
 }
