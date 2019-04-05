f98c9af7087c4b6074b9f2c7e7ab82fac5696ecf
n288TJYRX <n288TJYRX@tutanota.com>
2019-04-05 16:07:08 +0100

    Clicking an analytic now takes you to the parameters page

--- newui/src/app/analytic/analytic.component.ts
+++ newui/src/app/analytic/analytic.component.ts
@@ -13,7 +13,7 @@ export class AnalyticComponent implements OnInit {
   // Save the chosen analytic in the operationChain service (should change to use analyticsService)
   execute(operation) {
     this.operationChain.setOperation(operation);
-    this.navigation.goTo("parameters");
+    // this.navigation.goTo("parameters");
   }
 
   constructor() {}
