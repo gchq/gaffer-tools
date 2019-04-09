13cea11928d6ec534c7e2361004b7be2df5f3ef1
Guy Matthews <g609bmsma@protonmail.com>
2019-04-09 14:56:55 +0100

    edited data in table

--- newui/src/app/table/table.component.ts
+++ newui/src/app/table/table.component.ts
@@ -3,21 +3,18 @@ import { Component, OnInit } from "@angular/core";
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  population: number;
+  country: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
+  { position: 1, name: "Bristol", population: 1.0079, country: "UK" },
+  { position: 2, name: "Brighton", population: 4.0026, country: "UK" },
+  { position: 3, name: "Birmingham", population: 6.941, country: "UK" },
+  { position: 4, name: "Berlin", population: 9.0122, country: "DE" },
+  { position: 5, name: "Bruges", population: 10.811, country: "BE" },
+  { position: 6, name: "Beijing", population: 12.0107, country: "CH" },
+  { position: 7, name: "Winchester", population: 14.0067, country: "UK" }
 ];
 
 @Component({
@@ -25,7 +22,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: "./table.component.html"
 })
 export class TableComponent implements OnInit {
-  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  displayedColumns: string[] = ["position", "name", "country", "population"];
   dataSource = ELEMENT_DATA;
   constructor() {}
 
