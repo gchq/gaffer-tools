fe4ae383ae55dd2a686919ef2054a76a83f52263
Guy Matthews <g609bmsma@protonmail.com>
2019-04-11 15:42:19 +0100

    fixes

--- newui/src/app/analytic/analytic.component.ts
+++ newui/src/app/analytic/analytic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-analytic",
@@ -9,14 +9,14 @@ import { Router } from '@angular/router';
 export class AnalyticComponent implements OnInit {
   navigation;
   operationChain;
-  @Input('model') model;
+  @Input("model") model;
 
-  constructor(private router: Router) {};
+  constructor(private router: Router) {}
 
   // Save the chosen analytic in the operationChain service (should change to use analyticsService)
   execute(analytic) {
-    this.analyticService.setAnalytic(analytic);
-    this.router.navigate(['/parameters']);
+    // this.analyticService.setAnalytic(analytic);
+    this.router.navigate(["/parameters"]);
   }
 
   ngOnInit() {
