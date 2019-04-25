f2dbeb62315ddcc95c1fce0ad04c98492d45fca0
n288TJYRX <n288TJYRX@tutanota.com>
2019-04-25 13:24:02 +0100

    Cleaned code up on analytics and analytic components

--- newui/src/main/webapp/app/analytics/analytics.component.ts
+++ newui/src/main/webapp/app/analytics/analytics.component.ts
@@ -1,92 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Injectable } from "@angular/core";
 import { OperationService } from '../gaffer/operation.service';
+import { ErrorService } from '../dynamic-input/error.service';
 
 @Component({
   selector: "app-analytics",
   templateUrl: "./analytics.component.html",
   styleUrls: ["./analytics.component.css"]
 })
+@Injectable()
 export class AnalyticsComponent implements OnInit {
-  availableOperations;
 
-  constructor(private operationService: OperationService) {}
+  constructor(private operationService: OperationService,
+              private error: ErrorService) {}
 
   ngOnInit() {
-    this.reloadOperations(); // Load the operations
+    this.reloadOperations();
   }
-
-  //delete most of this left over from operation chain
-  populateOperations = function(availableOperations) {
-    this.availableOperations = [];
-
-    for (var i in availableOperations) {
-      var operation = availableOperations[i];
-
-      if (
-        !this.previous ||
-        !this.previous.selectedOperation ||
-        !this.previous.selectedOperation.next ||
-        this.previous.selectedOperation.next.indexOf(operation.class) > -1
-      ) {
-        operation.formattedName =
-          operation.name !== undefined
-            ? operation.name.toLowerCase().replace(/\s+/g, "")
-            : "";
-        operation.formattedDescription =
-          operation.description !== undefined
-            ? operation.description.toLowerCase().replace(/\s+/g, "")
-            : "";
-
-        if (operation.formattedName === "getelements") {
-          this.placeholder = "Search for an operation (e.g Get Elements)";
-        }
-        this.availableOperations.push(operation);
-      }
-    }
-
-    this.availableOperations.sort(function(a, b) {
-      if (a.namedOp && !b.namedOp) {
-        return -1;
-      }
-      if (!a.namedOp && b.namedOp) {
-        return 1;
-      }
-      if (a.formattedName > b.formattedName) {
-        return 1;
-      }
-      if (a.formattedName < b.formattedName) {
-        return -1;
-      }
-      if (a.formattedDescription > b.formattedDescription) {
-        return 1;
-      }
-      if (a.formattedDescription < b.formattedDescription) {
-        return -1;
-      }
-      return 0;
-    });
-
-    this.analytics = this.availableOperations;
-  };
-
-  // Delete this?
-  getOperations = function() {
-    return this.operationService
-      .getAvailableOperations(true)
-      .then(function(ops) {
-        this.populateOperations(ops);
-        return this.availableOperations;
-      });
-  };
-
-  // load the operations
+  
+  // load the analytics
   reloadOperations = function() {
     this.operationService.reloadOperations(true).subscribe(
-      availableOperations => {
-        console.log(availableOperations)
-        this.populateOperations(availableOperations)},
-      err => console.log(err));
-    //this.populateOperations()
-    //use error service to handle error
+      (availableAnalytics) => {
+        this.analytics = availableAnalytics;
+      },
+      (err) => {
+        this.error.handle(err);
+        console.log(err);
+      });
   };
 }
