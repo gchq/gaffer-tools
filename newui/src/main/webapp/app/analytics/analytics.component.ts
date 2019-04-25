29d5c99170d25dd24b95a24cddd864623827b9d9
n288TJYRX <n288TJYRX@tutanota.com>
2019-04-25 13:03:28 +0100

    Deleted dummy analytics data

--- newui/src/main/webapp/app/analytics/analytics.component.ts
+++ newui/src/main/webapp/app/analytics/analytics.component.ts
@@ -8,20 +8,12 @@ import { OperationService } from '../gaffer/operation.service';
 })
 export class AnalyticsComponent implements OnInit {
   availableOperations;
-  analytics = [[], []];
 
   constructor(private operationService: OperationService) {}
 
   ngOnInit() {
-    //this.analytics = analytics.reloadAnalytics();
     this.reloadOperations(); // Load the operations
   }
-
-  // analytics = [
-  //   "Get Adjacent Ids",
-  //   "Get All Elements",
-  //   "frequent-vehicles-in-region"
-  // ];
 
   //delete most of this left over from operation chain
   populateOperations = function(availableOperations) {
@@ -74,65 +66,6 @@ export class AnalyticsComponent implements OnInit {
       return 0;
     });
 
-    // Hard code the other data I assume we will get when we load up the analytic
-    var operationNames = ["Frequent Vehicles In Region", "Another Analytic"];
-    var descriptions = [
-      "Displays frequent vehicles in region as a table of results.",
-      "Another description"
-    ];
-    var operation1 = {
-      class: "uk.gov.gchq.gaffer.named.operation.NamedOperation",
-      fields: "some field",
-      parameters: [
-        "Result Limit",
-        "Start Date (YYYY-MM-DD)",
-        "End Date (YYYY-MM-DD)",
-        "Vehicle Type"
-      ],
-      operationName: "frequent-vehicles-in-region"
-    };
-    var operation2 = {
-      fields: {
-        input: "some input",
-        inputB: "some other input",
-        view: "some view"
-      }
-    };
-    var readAccessRoles = ["User", "User"];
-    var writeAccessRoles = ["User", "User"];
-    var operations = [operation1, operation2];
-    var parameters = [
-      [
-        "Result Limit",
-        "Start Date (YYYY-MM-DD)",
-        "End Date (YYYY-MM-DD)",
-        "Vehicle Type"
-      ]
-    ];
-    var outputTypes = ["table", "table"];
-    var score = [2, 3];
-    var color = ["red", "yellow"];
-    var icons = ["star", "directions_bus"]; //some names of icons from img folder
-
-    // Create the analytics from this hard coded data
-    var _i = 0;
-    for (_i = 0; _i < 2; _i++) {
-      //this.analytics[i].operation = availableOperations[i];
-
-      this.analytics[_i].operationName = operationNames[_i];
-      this.analytics[_i].description = descriptions[_i];
-      this.analytics[_i].operations = operations[_i];
-      this.analytics[_i].readAccessRoles = readAccessRoles[_i];
-      this.analytics[_i].writeAccessRoles = writeAccessRoles[_i];
-      this.analytics[_i].parameters = parameters[_i];
-      this.analytics[_i].outputType = [];
-      this.analytics[_i].outputType.output = outputTypes[_i];
-      this.analytics[_i].score = score[_i];
-      this.analytics[_i].color = color[_i];
-      this.analytics[_i].header = [];
-      this.analytics[_i].header.iconURL = icons[_i];
-    }
-    console.log(this.analytics);
     this.analytics = this.availableOperations;
   };
 
