4f3b117a64ec728a208ee45542a331fe87240596
n288TJYRX <n288TJYRX@tutanota.com>
2019-05-09 11:40:36 +0100

    Parameter input component now passes creation test

--- newui/src/main/webapp/app/parameter-input/parameter-input.component.spec.ts
+++ newui/src/main/webapp/app/parameter-input/parameter-input.component.spec.ts
@@ -1,6 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
 
 import { ParameterInputComponent } from './parameter-input.component';
+import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { AnalyticsService } from '../gaffer/analytics.service';
+
+@Component({
+  selector: 'app-query',
+  template: ''
+})
+class MockOperationComponent {
+  @Input() model;
+}
+
+class AnalyticsServiceStub {
+  getAnalytic = () => {
+    return {
+      operationName: 'Test operation name'
+    }
+  }
+}
 
 describe('ParameterInputComponent', () => {
   let component: ParameterInputComponent;
@@ -8,7 +28,18 @@ describe('ParameterInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParameterInputComponent ]
+      declarations: [ 
+        ParameterInputComponent,
+        MockOperationComponent
+       ],
+      imports: [
+        MatCardModule,
+        MatProgressSpinnerModule,
+        FormsModule
+      ],
+      providers: [
+        { provide: AnalyticsService, useClass: AnalyticsServiceStub}
+      ],
     })
     .compileComponents();
   }));
