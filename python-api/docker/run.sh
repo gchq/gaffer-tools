cd6b3b12d47f46e32a9837f70738812ced0b23d1
GCHQ-83497 <gchq-83497@gchq.gov.uk>
2019-05-09 11:58:56 +0100

    docker commands incorrect

--- python-api/docker/run.sh
+++ python-api/docker/run.sh
@@ -1,7 +1,7 @@
 # Builds and runs all of the containers together
 
-docker build -f Dockerfile.gaffer.quickstart.auth -f gaffer-quickstart-auth ../gafferpy-release-1.9.1-SNAPSHOT/
-docker build -f Dockerfile.gaffer.quickstart.pyspark -f gaffer-quickstart-pyspark ../gafferpy-release-1.9.1-SNAPSHOT/
-docker build -f Dockerfile.gaffer.quickstart.session -f gaffer-quickstart-session ../gafferpy-release-1.9.1-SNAPSHOT/
+docker build -f Dockerfile.gaffer.quickstart.pyspark -t gaffer-quickstart-pyspark ../gafferpy-release-1.9.1-SNAPSHOT/
+docker build -f Dockerfile.gaffer.quickstart.session -t gaffer-quickstart-session ../gafferpy-release-1.9.1-SNAPSHOT/
+docker build -f Dockerfile.gaffer.quickstart.auth -t gaffer-quickstart-auth .
 
 docker-compose up
