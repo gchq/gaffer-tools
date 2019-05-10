059049b9f5bc1637cc5c22b9525618c7820517e0
n288TJYRX <n288TJYRX@tutanota.com>
2019-05-10 17:09:19 +0100

    Adding doc link test to about component

--- newui/src/main/webapp/app/about/about.component.spec.ts
+++ newui/src/main/webapp/app/about/about.component.spec.ts
@@ -1,11 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { MatIconModule, MatCardModule, MatDividerModule } from '@angular/material';
+import { Router } from '@angular/router';
+import { CommonModule, Location } from '@angular/common';
+import { By } from '@angular/platform-browser';
 
 import { AboutComponent } from './about.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -13,19 +19,35 @@ describe('AboutComponent', () => {
       imports: [ 
         MatIconModule,
         MatCardModule,
-        MatDividerModule
+        MatDividerModule,
+        CommonModule,
+        RouterTestingModule
        ]
-    })
-    .compileComponents();
+    });
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(AboutComponent);
+    router.initialNavigation();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should open link to documentation', () => {
+    const debugElements = fixture.debugElement.queryAll(By.css('a'));
+    console.log(debugElements);
+    const index = debugElements.findIndex(de => de.attributes['href'] === 'https://gchq.github.io/gaffer-doc/');
+    console.log(index);
+    debugElements[index].nativeElement.click();
+    //fixture.detectChanges();
+    //tick();
+    expect(location.path()).toBe('/analytics');
   });
 });
