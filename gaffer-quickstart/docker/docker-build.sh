556b11ccabe834baa8c9f29003f7031c0d5a4d11
m316257 <m316257@users.noreply.github.com>
2019-07-04 20:39:39 +0100

    missed updates

--- gaffer-quickstart/docker/docker-build.sh
+++ gaffer-quickstart/docker/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile.gaffer.quickstart.base -t gaffer-quickstart-base ../gaffer-quickstart-release-1.8.4-SNAPSHOT/
+docker build -f Dockerfile.gaffer.quickstart.base -t gaffer-quickstart-base ../gaffer-quickstart-release-1.9.3-SNAPSHOT/
 
 docker build -f Dockerfile.gaffer.quickstart -t gaffer-quickstart .
 
