1795ed1705f1f6beda76653c6c8451b0220bcf8d
m316257 <m316257@users.noreply.github.com>
2019-07-04 20:12:32 +0100

    updated to 193-SNAPSHOT

--- gaffer-quickstart/quickstart-aws/src/env/env.sh
+++ gaffer-quickstart/quickstart-aws/src/env/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-QUICKSTART_VERSION="1.8.4-SNAPSHOT"
-GAFFER_SLIDER_VERSION="1.8.3"
-GAFFER_VERSION="1.8.3"
+QUICKSTART_VERSION="1.9.3-SNAPSHOT"
+GAFFER_SLIDER_VERSION="1.9.1"
+GAFFER_VERSION="1.9.1"
 
 CLUSTER_USAGE="50"
 ACCUMULO_VERSION="1.8.1"
@@ -10,7 +10,7 @@ INSTANCE_NAME="quickstart"
 REST_PORT="8085"
 TOMCAT_URL=https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.17/bin/apache-tomcat-9.0.17.tar.gz
 GAFFER_LIBS_HDFS=gaffer-libs/
-GAFFERPY_VERSION="1.8.4-SNAPSHOT"
+GAFFERPY_VERSION="1.9.3-SNAPSHOT"
 GAFFER_CONFIG_DIR="/home/hadoop/gaffer-config/"
 
 REST_OPERATION_DECLARATIONS="sparkAccumuloOperationsDeclarations.json,/home/hadoop/example/quickstartOperationDeclarations.json"
