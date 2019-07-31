f5efd08307b4adc460fcec13f750f54b8a960b90
d47853 <d47853@users.noreply.github.com>
2019-07-31 11:24:31 +0100

    WIP

--- cd/install.sh
+++ cd/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$RELEASE" != 'true' ]]; then
-    if [[ "$TRAVIS_BRANCH" == 'develop' ]] || [[ "$TRAVIS_PULL_REQUEST" != 'false' ]] && [[ "$MODULES" == 'analytics-ui' ]]; then
+    if [[ "$TRAVIS_BRANCH" == 'develop' ]] || [[ "$TRAVIS_PULL_REQUEST" != 'false' ]] && [[ "$MODULES" != 'analytics-ui' ]]; then
         if  [[ "$MODULES" == '' ]] || [[ $MODULES == *'!'* ]]; then
             echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
             mvn -q install -P quick,travis,build-extras -B -V
