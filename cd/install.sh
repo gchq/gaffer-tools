ad4926ddafc6bdd47fa3bab72b5c19b96b989d59
d47853 <d47853@users.noreply.github.com>
2019-07-31 10:27:24 +0100

    renamed angular cli dependency

--- cd/install.sh
+++ cd/install.sh
@@ -5,7 +5,7 @@ set -e
 if [[ "$RELEASE" != 'true' ]]; then
     if [[ "$TRAVIS_BRANCH" == 'develop' ]] || [[ "$TRAVIS_PULL_REQUEST" != 'false' ]]; then
         if [[ "$MODULES" == 'analytics-ui' ]]; then
-            npm install -g @angular-cli
+            npm install -g @angular/cli
             npm install
         elif  [[ "$MODULES" == '' ]] || [[ $MODULES == *'!'* ]]; then
             echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
