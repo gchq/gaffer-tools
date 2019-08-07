f1494c2c8914ed29cbc12a06e8da840044b52b9d
d47853 <d47853@users.noreply.github.com>
2019-08-07 13:15:51 +0100

    gh-645 moved core-site.xml to accumulo-store from hdfs-library

--- deployment/aws/tests/integration-tests/emr-step-scripts/run-gaffer-integration-tests.sh
+++ deployment/aws/tests/integration-tests/emr-step-scripts/run-gaffer-integration-tests.sh
@@ -127,9 +127,9 @@ for file in ./store-implementation/accumulo-store/src/test/resources/*.propertie
 done
 
 # Add the core-site.xml to make Gaffer use hdfs
-cp /etc/hadoop/conf/core-site.xml ./library/hdfs-library/src/test/resources
+cp /etc/hadoop/conf/core-site.xml ./store-implementation/accumulo-store/src/test/resources
 # Remove LZO codec to avoid class not found exceptions
-sed -i 's|<value>.*com\.hadoop\.compression\.lzo\.LzoCodec,com\.hadoop\.compression\.lzo\.LzopCodec.*</value>$|<value>org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,org.apache.hadoop.io.compress.BZip2Codec,org.apache.hadoop.io.compress.SnappyCodec</value>|' ./library/hdfs-library/src/test/resources/core-site.xml
+sed -i 's|<value>.*com\.hadoop\.compression\.lzo\.LzoCodec,com\.hadoop\.compression\.lzo\.LzopCodec.*</value>$|<value>org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,org.apache.hadoop.io.compress.BZip2Codec,org.apache.hadoop.io.compress.SnappyCodec</value>|' ./store-implementation/accumulo-store/src/test/resources/core-site.xml
 
 # Report test results script
 tee -a failsafe-report.py <<EOF
