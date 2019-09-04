c0b3bb688e19a2d24173a9b22125fdc7f5bcb24e
p013570 <p013570@users.noreply.github.com>
2019-09-04 07:48:35 +0100

    Set the title in defaultConfig

--- ui/src/main/webapp/app/config/config-service.js
+++ ui/src/main/webapp/app/config/config-service.js
@@ -67,7 +67,7 @@ angular.module('app').factory('config', ['$http', '$q', 'defaultRestEndpoint', '
                         }
                         angular.merge(mergedConfig, customConfig);
                         config = mergedConfig;
-                        document.title = config.title || 'Gaffer';
+                        document.title = config.title;
                         defer.resolve(config);
                     },
                     function(err) {
