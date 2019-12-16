68ca58cb90537d7f0c5eba4d41cce98c3e843a2d
g609bmsma <g609bmsma@protonmail.com>
2019-12-16 08:50:28 +0000

    copyright header added

--- analytics/analytics-ui/src/app/services/test/test.analytic.ts
+++ analytics/analytics-ui/src/app/services/test/test.analytic.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Analytic } from 'src/app/analytics/classes/analytic.class';
 
 const uiMappingDetail1 = {
@@ -6,7 +22,7 @@ const uiMappingDetail1 = {
     parameterName: 'param1',
     inputClass: 'java.lang.String',
     currentValue: 'value1'
-  };
+};
 const uiMappingDetail2 = {
     label: 'Label',
     userInputType: 'TextBox',
@@ -16,8 +32,8 @@ const uiMappingDetail2 = {
 };
 
 const uiMapping1 = {
-    key1 : uiMappingDetail1,
-    key2 : uiMappingDetail2,
+    key1: uiMappingDetail1,
+    key2: uiMappingDetail2,
 };
 
 const metaData1 = {
@@ -34,17 +50,17 @@ const options1 = new Map<string, string>();
 options1.set('option1', 'option1Value');
 
 const serialisedTestAnalytic = {
-    analyticName : 'test analytic name',
-    operationName : 'test operation name',
-    description : 'test description',
-    creatorId : 'test creator id',
-    readAccessRoles : [],
-    writeAccessRoles : [],
-    uiMapping : uiMapping1,
-    options : options1,
-    metaData : metaData1,
-    outputVisualisation : outputVisualisation1,
-    score : 5
+    analyticName: 'test analytic name',
+    operationName: 'test operation name',
+    description: 'test description',
+    creatorId: 'test creator id',
+    readAccessRoles: [],
+    writeAccessRoles: [],
+    uiMapping: uiMapping1,
+    options: options1,
+    metaData: metaData1,
+    outputVisualisation: outputVisualisation1,
+    score: 5
 };
 export { serialisedTestAnalytic };
 
