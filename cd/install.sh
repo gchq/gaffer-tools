ea4584adb70ad27837b9fba5d239e3131a9895ac
d47853 <d47853@users.noreply.github.com>
2020-01-16 18:08:25 +0000

    gh-814 added code to build from source

--- cd/install.sh
+++ cd/install.sh
@@ -4,6 +4,15 @@ set -e
 
 if [[ "$RELEASE" != 'true' ]]; then
     if [[ "$TRAVIS_BRANCH" == 'develop' ]] || [[ "$TRAVIS_PULL_REQUEST" != 'false' ]] && [[ "$MODULES" != 'analytics-ui' ]]; then
+        # Check if Gaffer version is in maven central and build from source if not
+        gaffer_version=`mvn help:evaluate -Dexpression=gaffer.version -q -DforceStdout`
+        mvn dependecy:get -Dartifact=uk.gov.gchq.gaffer:gaffer2:$(gaffer_version) -q
+        if [[ $? != 0 ]]; then
+            echo "Building Koryphe from source"
+            cd .. && git clone https://github.com/gchq/koryphe.git && cd koryphe && mvn install -Pquick -T2C -q
+            echo "Building Gaffer from source"
+            cd .. && git clone https://github.com/gchq/gaffer.git && cd gaffer && mvn install -Pquick -T2C -q
+        fi
         if  [[ "$MODULES" == '' ]] || [[ $MODULES == *'!'* ]]; then
             echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
             mvn -q install -P quick,travis,build-extras -B -V
