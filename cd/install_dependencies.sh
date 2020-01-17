65312e2d53f9d8cf79f533a04b7fe56ad24f220b
d47853 <d47853@users.noreply.github.com>
2020-01-17 12:46:20 +0000

    WIP@

--- cd/install_dependencies.sh
+++ cd/install_dependencies.sh
@@ -11,4 +11,7 @@ if [[ ${return_value} -ne 0 ]]; then
     cd .. && git clone https://github.com/gchq/koryphe.git && cd koryphe && mvn install -Pquick -q
     echo "Building Gaffer from source"
     cd .. && git clone https://github.com/gchq/gaffer.git && cd gaffer && mvn install -Pquick -q
+    ls -l ~/.m2/repository/uk/gov/gchq/gaffer
+    ls -l ~/.m2/repository/uk/gov/gchq/gaffer/gaffer2/
+    ls -l ~/.m2/repository/uk/gov/gchq/gaffer/gaffer2/1.10.5
 fi
