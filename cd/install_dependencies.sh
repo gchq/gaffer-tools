0f0967bf0d7e65a416ad447225f28b2451949419
d47853 <d47853@users.noreply.github.com>
2020-01-17 13:08:52 +0000

    WIP

--- cd/install_dependencies.sh
+++ cd/install_dependencies.sh
@@ -7,9 +7,9 @@ return_value=$?
 set -e
 if [[ ${return_value} -ne 0 ]]; then
     echo "Building Koryphe from source"
-    cd .. && git clone https://github.com/gchq/koryphe.git && cd koryphe && git checkout master && mvn dependency:purge-local-repository clean install -Pquick -q
+    cd .. && git clone https://github.com/gchq/koryphe.git && cd koryphe && git checkout master && mvn clean install -Pquick -q
     echo "Building Gaffer from source"
-    cd .. && git clone https://github.com/gchq/gaffer.git && cd gaffer && git checkout master && mvn dependency:purge-local-repository clean install -Pquick -q
+    cd .. && git clone https://github.com/gchq/gaffer.git && cd gaffer && git checkout master && mvn clean install -Pquick -q
     ls -l ~/.m2/repository/uk/gov/gchq/gaffer
     ls -l ~/.m2/repository/uk/gov/gchq/gaffer/gaffer2/
     ls -l ~/.m2/repository/uk/gov/gchq/gaffer/gaffer2/1.10.5
