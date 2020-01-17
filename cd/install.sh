7215900973bf28470bb9fc57fb6d9ad567f32e4e
d47853 <d47853@users.noreply.github.com>
2020-01-17 13:39:34 +0000

    Updating gaffer version 1.10.5 (#816)
    * Updated Gaffer version to 1.10.5
    
    * updated Travis.yml
    
    * added logging to identify travis issue
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * replaced travis_wait
    
    * WIP
    
    * WIP@
    
    * WIP
    
    * WIP
    
    * hopefully final WIP

--- cd/install.sh
+++ cd/install.sh
@@ -4,19 +4,7 @@ set -e
 
 if [[ "$RELEASE" != 'true' ]]; then
     if [[ "$TRAVIS_BRANCH" == 'develop' ]] || [[ "$TRAVIS_PULL_REQUEST" != 'false' ]] && [[ "$MODULES" != 'analytics-ui' ]]; then
-        # Check if Gaffer version is in maven central and build from source if not
-        gaffer_version=`mvn help:evaluate -Dexpression=gaffer.version -q -DforceStdout`
-        set +e
-        mvn dependency:get -Dartifact=uk.gov.gchq.gaffer:gaffer2:${gaffer_version} -q -Dpackaging=pom
-        return_value=$?
-        set -e
-        if [[ ${return_value} -ne 0 ]]; then
-            echo "Building Koryphe from source"
-            cd .. && git clone https://github.com/gchq/koryphe.git && cd koryphe && mvn install -Pquick -q
-            echo "Building Gaffer from source"
-            cd .. && git clone https://github.com/gchq/gaffer.git && cd gaffer && mvn install -Pquick -q
-            cd ../gaffer-tools
-        fi
+        ./cd/install_dependencies.sh
         if  [[ "$MODULES" == '' ]] || [[ $MODULES == *'!'* ]]; then
             echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
             mvn -q install -P quick,travis,build-extras -B -V
