58085a98a5bfb5c06956dccf1a921e1aaacfd6c4
j8934893 <67467499+j8934893@users.noreply.github.com>
2020-09-17 12:57:42 +0100

    Gh 845 is operation supported null input (#846)
    * Updated the is_operation_supported method to handle operation=None. Added a detailed docstring.
    
    * Added a test for the is_operation_supported method
    
    * Updated the doc string for execute_get
    
    * Updated the doc string for execute_get
    
    * Created a test for execute_get
    
    * Updated the doc string for execute_get
    
    * Changed the output of execute_get and is_operation_supported from str(JSON) to JSON.
    
    * Minor docstring updates
    
    * Made the operation parameter mandatory for is_operation_supported().
    Minor docstring updates.
    
    * Updated tests to correctly compare expected and actual results.
    
    * added newline to end of file
    
    Co-authored-by: d47853 <d47853@users.noreply.github.com>

--- python-shell/src/test/test_connector.py
+++ python-shell/src/test/test_connector.py
@@ -15,6 +15,7 @@
 #
 
 import unittest
+import json
 
 from gafferpy import gaffer as g
 from gafferpy import gaffer_connector
@@ -43,6 +44,189 @@ class GafferConnectorTest(unittest.TestCase):
                     "SOURCE")],
             elements)
 
+    def test_is_operation_supported(self):
+        gc = gaffer_connector.GafferConnector('http://localhost:8080/rest/latest')
+
+        response_text = gc.is_operation_supported(
+            g.IsOperationSupported(
+                operation='uk.gov.gchq.gaffer.operation.impl.get.GetAllElements'
+            )
+        )
+
+        expected_response_text = '''
+        {
+          "name": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements",
+          "summary": "Gets all elements compatible with a provided View",
+          "fields": [
+            {
+              "name": "view",
+              "className": "uk.gov.gchq.gaffer.data.elementdefinition.view.View",
+              "required": false
+            },
+            {
+              "name": "options",
+              "className": "java.util.Map<java.lang.String,java.lang.String>",
+              "required": false
+            },
+            {
+              "name": "directedType",
+              "summary": "Is the Edge directed?",
+              "className": "java.lang.String",
+              "options": [
+                "DIRECTED",
+                "UNDIRECTED",
+                "EITHER"
+              ],
+              "required": false
+            },
+            {
+              "name": "views",
+              "className": "java.util.List<uk.gov.gchq.gaffer.data.elementdefinition.view.View>",
+              "required": false
+            }
+          ],
+          "next": [
+            "uk.gov.gchq.gaffer.operation.impl.add.AddElements",
+            "uk.gov.gchq.gaffer.operation.impl.get.GetElements",
+            "uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds",
+            "uk.gov.gchq.gaffer.operation.impl.export.set.ExportToSet",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToArray",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToEntitySeeds",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToList",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToMap",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToCsv",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToSet",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToStream",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToVertices",
+            "uk.gov.gchq.gaffer.named.operation.NamedOperation",
+            "uk.gov.gchq.gaffer.operation.impl.compare.Max",
+            "uk.gov.gchq.gaffer.operation.impl.compare.Min",
+            "uk.gov.gchq.gaffer.operation.impl.compare.Sort",
+            "uk.gov.gchq.gaffer.operation.impl.GetWalks",
+            "uk.gov.gchq.gaffer.operation.impl.generate.GenerateElements",
+            "uk.gov.gchq.gaffer.operation.impl.generate.GenerateObjects",
+            "uk.gov.gchq.gaffer.operation.impl.Validate",
+            "uk.gov.gchq.gaffer.operation.impl.Count",
+            "uk.gov.gchq.gaffer.operation.impl.CountGroups",
+            "uk.gov.gchq.gaffer.operation.impl.Limit",
+            "uk.gov.gchq.gaffer.operation.impl.DiscardOutput",
+            "uk.gov.gchq.gaffer.operation.impl.Map",
+            "uk.gov.gchq.gaffer.operation.impl.If",
+            "uk.gov.gchq.gaffer.operation.impl.While",
+            "uk.gov.gchq.gaffer.operation.impl.ForEach",
+            "uk.gov.gchq.gaffer.operation.impl.output.ToSingletonList",
+            "uk.gov.gchq.gaffer.operation.impl.Reduce",
+            "uk.gov.gchq.gaffer.operation.impl.join.Join",
+            "uk.gov.gchq.gaffer.operation.impl.SetVariable",
+            "uk.gov.gchq.gaffer.operation.impl.function.Filter",
+            "uk.gov.gchq.gaffer.operation.impl.function.Transform",
+            "uk.gov.gchq.gaffer.operation.impl.function.Aggregate",
+            "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsBetweenSets",
+            "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsWithinSet",
+            "uk.gov.gchq.gaffer.operation.impl.SplitStoreFromIterable",
+            "uk.gov.gchq.gaffer.operation.impl.SampleElementsForSplitPoints",
+            "uk.gov.gchq.gaffer.accumulostore.operation.impl.SummariseGroupOverRanges",
+            "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsInRanges",
+            "uk.gov.gchq.gaffer.operation.export.graph.ExportToOtherAuthorisedGraph",
+            "uk.gov.gchq.gaffer.operation.export.graph.ExportToOtherGraph",
+            "uk.gov.gchq.gaffer.operation.impl.export.resultcache.ExportToGafferResultCache"
+          ],
+          "exampleJson": {
+            "class": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements"
+          },
+          "outputClassName": "uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable<uk.gov.gchq.gaffer.data.element.Element>"
+        }
+            '''
+
+        self.assertEqual(
+            json.loads(expected_response_text),
+            json.loads(response_text)
+        )
+
+    def test_execute_get(self):
+        self.maxDiff = None
+        gc = gaffer_connector.GafferConnector('http://localhost:8080/rest/latest')
+
+        response_text = gc.execute_get(
+            g.GetOperations()
+        )
+
+        expected_response_text = '''
+        [
+          "uk.gov.gchq.gaffer.operation.impl.add.AddElements",
+          "uk.gov.gchq.gaffer.operation.impl.get.GetElements",
+          "uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds",
+          "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements",
+          "uk.gov.gchq.gaffer.operation.impl.export.set.ExportToSet",
+          "uk.gov.gchq.gaffer.operation.impl.export.set.GetSetExport",
+          "uk.gov.gchq.gaffer.operation.impl.export.GetExports",
+          "uk.gov.gchq.gaffer.operation.impl.job.GetJobDetails",
+          "uk.gov.gchq.gaffer.operation.impl.job.GetAllJobDetails",
+          "uk.gov.gchq.gaffer.operation.impl.job.GetJobResults",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToArray",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToEntitySeeds",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToList",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToMap",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToCsv",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToSet",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToStream",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToVertices",
+          "uk.gov.gchq.gaffer.named.operation.NamedOperation",
+          "uk.gov.gchq.gaffer.named.operation.AddNamedOperation",
+          "uk.gov.gchq.gaffer.named.operation.GetAllNamedOperations",
+          "uk.gov.gchq.gaffer.named.operation.DeleteNamedOperation",
+          "uk.gov.gchq.gaffer.named.view.AddNamedView",
+          "uk.gov.gchq.gaffer.named.view.GetAllNamedViews",
+          "uk.gov.gchq.gaffer.named.view.DeleteNamedView",
+          "uk.gov.gchq.gaffer.operation.impl.compare.Max",
+          "uk.gov.gchq.gaffer.operation.impl.compare.Min",
+          "uk.gov.gchq.gaffer.operation.impl.compare.Sort",
+          "uk.gov.gchq.gaffer.operation.OperationChain",
+          "uk.gov.gchq.gaffer.operation.OperationChainDAO",
+          "uk.gov.gchq.gaffer.operation.impl.ValidateOperationChain",
+          "uk.gov.gchq.gaffer.operation.impl.GetWalks",
+          "uk.gov.gchq.gaffer.operation.impl.generate.GenerateElements",
+          "uk.gov.gchq.gaffer.operation.impl.generate.GenerateObjects",
+          "uk.gov.gchq.gaffer.operation.impl.Validate",
+          "uk.gov.gchq.gaffer.operation.impl.Count",
+          "uk.gov.gchq.gaffer.operation.impl.CountGroups",
+          "uk.gov.gchq.gaffer.operation.impl.Limit",
+          "uk.gov.gchq.gaffer.operation.impl.DiscardOutput",
+          "uk.gov.gchq.gaffer.store.operation.GetSchema",
+          "uk.gov.gchq.gaffer.operation.impl.Map",
+          "uk.gov.gchq.gaffer.operation.impl.If",
+          "uk.gov.gchq.gaffer.operation.impl.While",
+          "uk.gov.gchq.gaffer.operation.impl.ForEach",
+          "uk.gov.gchq.gaffer.operation.impl.output.ToSingletonList",
+          "uk.gov.gchq.gaffer.operation.impl.Reduce",
+          "uk.gov.gchq.gaffer.operation.impl.join.Join",
+          "uk.gov.gchq.gaffer.operation.impl.job.CancelScheduledJob",
+          "uk.gov.gchq.gaffer.operation.impl.SetVariable",
+          "uk.gov.gchq.gaffer.operation.impl.GetVariable",
+          "uk.gov.gchq.gaffer.operation.impl.GetVariables",
+          "uk.gov.gchq.gaffer.operation.impl.function.Filter",
+          "uk.gov.gchq.gaffer.operation.impl.function.Transform",
+          "uk.gov.gchq.gaffer.operation.impl.function.Aggregate",
+          "uk.gov.gchq.gaffer.store.operation.GetTraits",
+          "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsBetweenSets",
+          "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsWithinSet",
+          "uk.gov.gchq.gaffer.operation.impl.SplitStoreFromFile",
+          "uk.gov.gchq.gaffer.operation.impl.SplitStoreFromIterable",
+          "uk.gov.gchq.gaffer.operation.impl.SampleElementsForSplitPoints",
+          "uk.gov.gchq.gaffer.accumulostore.operation.impl.SummariseGroupOverRanges",
+          "uk.gov.gchq.gaffer.accumulostore.operation.impl.GetElementsInRanges",
+          "uk.gov.gchq.gaffer.operation.export.graph.ExportToOtherAuthorisedGraph",
+          "uk.gov.gchq.gaffer.operation.export.graph.ExportToOtherGraph",
+          "uk.gov.gchq.gaffer.operation.impl.export.resultcache.ExportToGafferResultCache",
+          "uk.gov.gchq.gaffer.operation.impl.export.resultcache.GetGafferResultCacheExport"
+        ]
+        '''
+
+        self.assertEqual(
+            json.loads(expected_response_text),
+            json.loads(response_text)
+        )
+        
     def test_dummy_header(self):
         """Test that the addition of a dummy header does not effect the standard test"""
         gc = gaffer_connector.GafferConnector('http://localhost:8080/rest/latest', headers={"dummy_Header": "value"})
