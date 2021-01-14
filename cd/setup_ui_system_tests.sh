9cb3219ec67635d55c6faa2c115171f4faa1c817
d47853 <d47853@users.noreply.github.com>
2021-01-14 14:34:57 +0000

    Setup Dbus

--- cd/setup_ui_system_tests.sh
+++ cd/setup_ui_system_tests.sh
@@ -6,4 +6,7 @@ mvn install -P quick,road-traffic-demo -B -pl ui &
 curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 tar -xf geckodriver-v0.23.0-linux64.tar.gz
 mv geckodriver ui/
-sleep 1m
+sudo apt-get install -y dbus
+sudo bash -c "dbus-uuidgen > /etc/machine-id"
+sleep 30s
+
