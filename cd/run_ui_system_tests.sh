8066e339281bed65d87f3f2d1eeb18a816ad51e6
d47853 <d47853@users.noreply.github.com>
2021-01-14 14:44:13 +0000

    Reduced amount of tests run with xvfb

--- cd/run_ui_system_tests.sh
+++ cd/run_ui_system_tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-mvn verify -P system-test -Dwebdriver.gecko.driver=geckodriver -pl ui
-cd python-shell && python3 -m unittest discover -s src
+
+
--- cd/setup_ui_system_tests.sh
+++ cd/setup_ui_system_tests.sh
@@ -6,7 +6,5 @@ mvn install -P quick,road-traffic-demo -B -pl ui &
 curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 tar -xf geckodriver-v0.23.0-linux64.tar.gz
 mv geckodriver ui/
-sudo apt-get install -y dbus
-sudo bash -c "dbus-uuidgen > /etc/machine-id"
-sleep 30s
+sleep 1m
 
