f1ab4465e66df6b45c9d6621f17c8b4e31d40c71
d47853 <d47853@users.noreply.github.com>
2021-01-14 13:05:23 +0000

    Removed installs

--- cd/system-test-ui.sh
+++ cd/system-test-ui.sh
@@ -3,11 +3,6 @@
 set -e
 
 mvn install -P quick,travis,road-traffic-demo -B -pl ui &
-export DISPLAY=:99.0
-sh -e xvfb start
-sudo apt-get -qq update
-sudo apt-get install -y dbus
-sudo bash -c "dbus-uuidgen > /etc/machine-id"
 curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 tar -xf geckodriver-v0.23.0-linux64.tar.gz
 mv geckodriver ui/
