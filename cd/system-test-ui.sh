507fe9a33931dc368d71d2bbaadd366bc7782444
d47853 <d47853@users.noreply.github.com>
2021-01-14 13:40:18 +0000

    Removed non existent profile

--- cd/system-test-ui.sh
+++ cd/system-test-ui.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mvn install -P quick,travis,road-traffic-demo -B -pl ui &
+mvn install -P quick,road-traffic-demo -B -pl ui &
 curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 tar -xf geckodriver-v0.23.0-linux64.tar.gz
 mv geckodriver ui/
