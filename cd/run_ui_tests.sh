398b3fe38e7ac3c0e766c5ee2757946a750fd823
d47853 <d47853@users.noreply.github.com>
2021-01-15 17:14:54 +0000

    Removed cleanup code

--- cd/run_ui_tests.sh
+++ cd/run_ui_tests.sh
@@ -4,10 +4,10 @@ set -e
 
 xvfb-run --auto-servernum mvn clean install -pl :ui
 
-xvfb_pids=`ps aux | grep tmp/xvfb-run | grep -v grep | awk '{print $2}'`
-if [ "$xvfb_pids" != "" ]; then
-  echo "Killing the following xvfb processes: $xvfb_pids"
-  sudo kill $xvfb_pids
-else
-  echo "No xvfb processes to kill"
-fi
+#xvfb_pids=`ps aux | grep tmp/xvfb-run | grep -v grep | awk '{print $2}'`
+#if [ "$xvfb_pids" != "" ]; then
+#  echo "Killing the following xvfb processes: $xvfb_pids"
+#  sudo kill -9 $xvfb_pids
+#else
+#  echo "No xvfb processes to kill"
+#fi
