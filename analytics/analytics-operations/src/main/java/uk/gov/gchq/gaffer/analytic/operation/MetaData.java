c374780001e7cc4a6a8d550b83513c6d4a916727
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2022-02-08 10:54:38 +0000

    Fix/Update Copyright headers

--- analytics/analytics-operations/src/main/java/uk/gov/gchq/gaffer/analytic/operation/MetaData.java
+++ analytics/analytics-operations/src/main/java/uk/gov/gchq/gaffer/analytic/operation/MetaData.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.analytic.operation;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
--- analytics/analytics-operations/src/main/java/uk/gov/gchq/gaffer/analytic/operation/serialisation/AnalyticTypeReference.java
+++ analytics/analytics-operations/src/main/java/uk/gov/gchq/gaffer/analytic/operation/serialisation/AnalyticTypeReference.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.analytic.operation.serialisation;
 
 import com.fasterxml.jackson.core.type.TypeReference;
