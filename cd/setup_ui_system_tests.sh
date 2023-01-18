a9160501275312fb5a6afec11492adc3d487f573
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2023-01-18 16:34:46 +0000

    Remove UI

--- cd/setup_ui_system_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mvn install -P quick,road-traffic-demo -B -pl ui &
-curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz
-tar -xf geckodriver-v0.31.0-linux64.tar.gz
-mv geckodriver ui/
-sleep 1m
